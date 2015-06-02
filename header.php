d97c89377e23c1495f0b9e3450c5d117cf075fc5
irthos <irth03@gmail.com>
2015-06-02 12:22:03 -0700

    ending selina, starting nimet

--- header.php
+++ header.php
@@ -18,8 +18,8 @@
 <header class="clearfix">
     <nav class="navbar navbar-default top-nav nomar ">
         <a href="<?php get_site_url(); ?>" class="col-xs-12 col-sm-3">
-            <img class="hidden-xs" src="wp-content/uploads/2015/06/lesmaisons-logo.png" alt=""/>
-            <img class="visible-xs" src="wp-content/uploads/2015/06/lesmaisons-logo-long.png" alt=""/>
+            <img class="hidden-xs" src="/wp-content/uploads/2015/06/lesmaisons-logo.png" alt=""/>
+            <img class="visible-xs" src="/wp-content/uploads/2015/06/lesmaisons-logo-long.png" alt=""/>
         </a>
         <div class="col-xs-12 col-sm-9" ng-init="nav.pages = [['About','info'],['Suites','home'],['Contact','phone'],['Explore','street-view']]">
             <div class="navbar-text col-xs-12">
