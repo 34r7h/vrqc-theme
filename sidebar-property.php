1b279648cb5f6f147cf10986b19c0aea46002465
irthos <irth03@gmail.com>
2015-06-03 15:09:12 -0700

    payment form and footer tight

--- sidebar-property.php
+++ sidebar-property.php
@@ -1,5 +1,5 @@
 <?php if ( is_active_sidebar( 'sidebar-property' ) ) : ?>
-<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+<div id="primary-sidebar" class="primary-sidebar widget-area light-text" role="complementary">
     <?php dynamic_sidebar( 'sidebar-property' ); ?>
 </div><!-- #primary-sidebar -->
 <?php endif; ?>
