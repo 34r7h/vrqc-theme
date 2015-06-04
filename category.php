339d749f7e06135eef8fa304b5b5aa44c3dfed2c
irthos <irth03@gmail.com>
2015-06-03 17:55:10 -0700

    payment form and footer tight

--- category.php
+++ category.php
@@ -1,9 +1,9 @@
 <div class="clearfix"><?php get_header(); ?></div>
 <h1><?php single_cat_title(); ?></h1>
 <div class="clearfix">
-    <section class="col-sm-8"><?php
+    <section class="col-xs-12"><?php
 while ( have_posts() ) : the_post();
-    echo '<a class="col-xs-12 col-sm-6" href="'. get_permalink() .'"><div class="panel panel-dark post-list"><div class="panel-heading"><i class="fa fa-thumb-tack fa-2x"></i> ';
+    echo '<a class="col-xs-6 col-sm-4 col-md-3" href="'. get_permalink() .'"><div class="panel panel-dark post-list"><div class="panel-heading"><i class="fa fa-thumb-tack fa-2x"></i> ';
         echo the_title().'</div>';
         echo '<div class="panel-body">'.get_the_post_thumbnail().'</div>';
         echo '</div></a>';
@@ -15,10 +15,7 @@ while ( have_posts() ) : the_post();
         wp_reset_query();
 
         ?></section>
-    <aside class="col-xs-12 col-sm-4">
-        <?php get_search_form(); ?>
-        <?php get_sidebar(); ?>
-    </aside>
+
 </div>
 <div class="clearfix"><?php get_footer(); ?>
 </div>
