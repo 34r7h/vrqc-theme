23a85500c136f94a097aeb30414673f58dfe13eb
irthos <irth03@gmail.com>
2015-06-03 17:32:56 -0700

    payment form and footer tight

--- header.php
+++ header.php
@@ -45,8 +45,8 @@
                     </div>
                 </span>
                 <div ng-mouseleave="suitesDropdown['Suites']=false;" ng-if="suitesDropdown['Suites']" class="btn-group btn-group-vertical nopad" style="position: absolute; width: 75%; top: 1.6em; z-index: 99999; left:21%">
-                    <a ng-href="{{property.url}}" class="text-left btn btn-default " ng-repeat="property in vrqc.propertyPosts.posts" style="height:35px">
-                        <img class="col-xs-2" width="100%" ng-src="{{property.thumbnail}}">
+                    <a ng-href="{{property.url}}" class="text-left btn btn-default " ng-repeat="property in vrqc.propertyPosts.posts" style="height:37px">
+                        <img class="col-xs-1 nopad nomar" width="100%" ng-src="{{property.thumbnail}}">
                         <div class="col-xs-9 text-left">{{property.title}}</div>
                         <div class="text-left pull-left">{{property.custom_fields.roomcount[0]}} br</div>
 
