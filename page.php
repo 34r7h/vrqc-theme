69687c6d4f9a260790e4f2f19d6ddfed15e45df9
irthos <irth03@gmail.com>
2015-06-03 18:03:25 -0700

    payment form and footer tight

--- page.php
+++ page.php
@@ -4,15 +4,15 @@
 <div class="clearfix">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <article class="col-xs-12 col-sm-8" id="content" role="main">
+    <article class="col-xs-12" id="content" role="main">
         <div class="panel panel-default">
-            <h2 class="panel-heading">
-                <?php
+            <div class="panel-heading">
+                <h2><?php
                 $post_id = url_to_postid($url);
                 $page = get_post($post_id);
                 echo $page->post_title;
-                ?>
-            </h2>
+                ?></h2>
+            </div>
             <div class="panel-body">
                 <div class="col-xs-6">
                     <?php echo get_the_post_thumbnail() ?>
@@ -21,9 +21,7 @@
             </div>
         </div>
     </article>
-    <aside class="col-xs-12 col-sm-4">
-        <?php get_sidebar(); ?>
-    </aside></div>
+    </div>
 
 
 <?php endwhile; endif; ?>
